6508faea2e897afeda279a71453d53d6777d1a5f
Masanori Beppu <yaaman01@mbp-mb.local>
2020-08-25 18:18:48 +0900

    ruby_kadai2_1

--- task.rb
+++ task.rb
@@ -155,7 +155,19 @@ end
 
 class UserQ17
   # 以下に回答を記載
+  attr_accessor :name, :age, :gender
 
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+
+  def info
+    puts "名前：#{@name}"
+    puts "年齢:#{@age}"
+    puts "性別:#{@gender}"
+  end
 end
 
 def q17
@@ -170,7 +182,17 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  attr_accessor :name, :age
 
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    puts "こんにちは、#{user1[@name]}と申します。宜しくお願いいたします。"
+    puts "はいさまいど~、#{user2[@name]}です！！！"
+  end
 end
 
 def q18
@@ -184,10 +206,16 @@ end
 
 class Item
   # 以下を修正して下さい
+  attr_accessor :name
 
   def initialize(name)
     @name = name
   end
+
+  def name
+    return "ゼロ秒思考"
+  end
+
 end
 
 def q19
@@ -198,12 +226,24 @@ end
 
 class UserQ20
   # 以下に回答を記載
-
+  attr_accessor :name, :age
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
 end
 
 class Zoo
   # 以下に回答を記載
+  attr_accessor :infant, :children, :about, :sinior
+  def initialize(infant, children, about, sinior)
+    @infant = infant
+    @childrem = children
+    @about = about
+    @sinior = sinior
+  end
 
+  
 end
 
 
